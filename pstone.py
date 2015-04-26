76f221433797d7865336a40bfdd5a6d77a723744
robertding <robertdingx@gmail.com>
2015-04-26 22:08:13 +0800

    [add] bisic file

---
+++ pstone.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#   Author  :   RobertDing
+#   E-mail  :   robertdingx@gmail.com
+#   Date    :   15/04/26 16:41:03
+#   Desc    :   两周自制脚本语言 python 实现版本
+#
+from __future__ import absolute_import, division, with_statement
+
+
+class Token:
+    mumber = None
+    strings = None
+    opt = None
