c6900b592248a6992ba15eed839732199b6a2ff4
robertding <robertdingx@gmail.com>
2015-05-04 20:20:48 +0800

    [add] 构造ast语法树

--- pstone.py
+++ pstone.py
@@ -158,3 +158,87 @@ class Lexer(object):
             splash = False
             tostring += c
         return tostring
+
+
+class ASTree(object):
+    def child(self, i):
+        raise NotImplementedError
+
+    def num_children(self):
+        raise NotImplementedError
+
+    def location(self):
+        raise NotImplementedError
+
+    def __iter__(self):
+        raise NotImplementedError
+
+
+def ASTLeaf(ASTree):
+    def __init__(self, token):
+        self.token = token
+
+    def num_children(self):
+        return 0
+
+    def to_string(self):
+        return self.token.to_string()
+
+    def location(self):
+        return "at line " + self.token.line_number
+
+    def __iter__(self):
+        return None
+
+
+def ASTList(ASTree):
+    def __init__(self, astlist):
+        self.astlist = astlist or []
+
+    def child(self, i):
+        return self.astlist[i]
+
+    def num_children(self):
+        return len(self.astlist)
+
+    def __iter__(self):
+        return self.astlist.__iter__
+
+    def to_string(self):
+        return ' '.join(t.to_string() for t in self.astlist)
+
+    def location(self):
+        for i in self.astlist:
+            if i.location() is not None:
+                return i.location()
+        return None
+
+
+def NumLiteral(ASTLeaf):
+    def __init__(self, token):
+        super(ASTLeaf, self).__init__(token)
+
+    def value(self):
+        return self.token.num
+
+
+def Name(ASTLeaf):
+    def __init__(self, token):
+        super(ASTLeaf, self).__init__(token)
+
+    def name(self):
+        return self.token.to_string()
+
+
+class BinaryExpr(ASTList):
+    def __init__(self, astlist):
+        super(ASTList, self).__init__(astlist)
+
+    def left(self):
+        return self.child(0)
+
+    def right(self):
+        return self.child(2)
+
+    def operator(self):
+        return self.child(1).token.to_string()
