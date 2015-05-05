86e50869bdb45e7136586b7d2267734d519ee929
robertding <robertdingx@gmail.com>
2015-05-06 01:03:58 +0800

    [fix] runner

--- runner.py
+++ runner.py
@@ -10,16 +10,32 @@ from __future__ import absolute_import, division, with_statement
 
 import sys
 
-import pstone
+import token
+import basicParser
+
+
+parse_file = open(sys.argv[1], 'r')
+lexer = token.Lexer(parse_file)
+
+
+def lexer_runner():
+    res = lexer.read()
+    while res is not token.Token.EOF:
+        print("==> " + res.to_string())
+        res = lexer.read()
+
+
+def parser_runner():
+    parser = basicParser.BasicParser()
+    while lexer.peek(0) != token.Token.EOF:
+        ast = parser.parse(lexer)
+        print("==>  " + ast.to_string())
 
 
 def main():
-    parse_file = open(sys.argv[1], 'r')
-    parse = pstone.Lexer(parse_file)
-    res = parse.read()
-    while res is not pstone.Token.EOF:
-        print("==> " + res.to_string())
-        res = parse.read()
+    run = dict(lexer=lexer_runner,
+               parser=parser_runner)
+    run.get(sys.argv.get(1), lambda x: x)()
 
 if __name__ == '__main__':
     main()
